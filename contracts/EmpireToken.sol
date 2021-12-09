4394a36e59c736388a4f43d82d6edff5575cac54
Victor Wu <inexplicableeth@gmail.com>
2021-12-08 19:20:11 -0800

    initial commit

---
+++ contracts/EmpireToken.sol
@@ -0,0 +1,43 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract EmpireToken is
+    ERC20("Empire", "EMPIRE"),
+    ERC20Burnable,
+    ERC20Snapshot,
+    Ownable,
+    Pausable
+{
+    uint256 public MAX_SUPPLY = 1 * 10**9 * 18**18; //1billion
+
+    constructor() {
+        _mint(msg.sender, MAX_SUPPLY);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function snapshot() external onlyOwner {
+        _snapshot();
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Snapshot) whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,11 +5,11 @@ import {
 import { ethers } from "hardhat"
 
 const main = async(): Promise<any> => {
-  const Coin: ContractFactory = await ethers.getContractFactory("ExampleERC20")
-  const coin: Contract = await Coin.deploy()
+  const EmpireToken: ContractFactory = await ethers.getContractFactory("EmpireToken")
+  const empireToken: Contract = await EmpireToken.deploy()
 
-  await coin.deployed()
-  console.log(`Coin deployed to: ${coin.address}`)
+  await empireToken.deployed()
+  console.log(`EmpireToken deployed to: ${empireToken.address}`)
 }
 
 main()
